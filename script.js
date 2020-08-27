c4ac63c520751886d1e203a37ed6f1206e072c2e
tleach01 <taylorleach0@gmail.com>
2020-08-26 22:23:48 -0400

    not even close

--- script.js
+++ script.js
@@ -34,3 +34,30 @@ const currencies = [
   'MYR'
 ]
 
+const fromCurrency = document.getElementById('#fromCurrency')
+const fromAmount = document.getElementById('#fromAmount')
+const toCurrency = document.getElementById('#toCurrency')
+const toAmount = document.getElementById('#toAmount')
+const rate = document.getElementById('#rate')
+
+fromCurrency.addEventListener('change')
+fromAmount.addEventListener('change')
+toCurrency.addEventListener('change')
+toAmount.addEventListener('change')
+
+function calculate () {
+  const fromCurrency = fromCurrency.value
+  const toCurrency = toCurrency.value
+  fetch('https://api.exchangeratesapi.io/latest')
+    .then( function (response) {
+      return Response.json()
+    })
+
+calculate()
+
+
+const convert = document.querySelector('#convert-button')
+convert.addEventListener('click', function (event) {
+  event.preventDefault()
+}
+)
